4c4b30ff04a8b17c4848b900c6c841ba2592f62f
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2024-02-09 14:26:26 +0300

    navbar, footer and layout edited

---
+++ src/components/sidebar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Mete Furkan</h1>
+      </div>
+      <ul>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
