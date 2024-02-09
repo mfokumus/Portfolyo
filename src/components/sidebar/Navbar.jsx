e14a455dcf33cd00f354c13a4ff8e7c1929d962f
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2024-02-09 14:40:10 +0300

    navbar is prepared

--- src/components/sidebar/Navbar.jsx
+++ src/components/sidebar/Navbar.jsx
@@ -1,29 +1,52 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import {
+  IoHome,
+  IoPerson,
+  IoList,
+  IoBriefcase,
+  IoReceipt,
+} from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="bg-white">
+      <header>
         <h1>Mete Furkan</h1>
-      </div>
-      <ul>
-        <li>
-          <Link></Link>
-        </li>
-        <li>
-          <Link></Link>
-        </li>
-        <li>
-          <Link></Link>
-        </li>
-        <li>
-          <Link></Link>
-        </li>
-        <li>
-          <Link></Link>
-        </li>
-      </ul>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link>
+              <IoHome />
+              <span>Anasayfa</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <IoPerson />
+              <span>Hakkımızda</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <IoList />
+              <span>Hizmetlerim</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <IoBriefcase />
+              <span>Portfolyom</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <IoReceipt />
+              <span>İletişim</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
